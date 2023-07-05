de6937dd4942a0092034acc9fe80fecba0f19cd8
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-05 16:08:49 +0200

    add pieces components and fetch

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import useSWR from "swr";
+import Artpieces from "../components/ArtPieces/ArtPieces";
+
 export default function HomePage() {
+  const { data, isLoading, error } = useSWR(
+    "https://example-apis.vercel.app/api/art"
+  );
+  console.log("data:", data);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>{error.message}</h1>;
+  }
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <Artpieces pieces={data} />
     </div>
   );
 }
